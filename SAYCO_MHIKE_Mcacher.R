88fb38cae978baf4558f88f91e76e06c796a3877
MhikeSayco <91795487+MhikeSayco@users.noreply.github.com>
2021-10-03 00:54:30 +0800

    Add files via upload

---
+++ SAYCO_MHIKE_Mcacher.R
@@ -0,0 +1,18 @@
+%{
+  This program converts given number of weeks to seconds
+  Written by: Mhike Ysrael P. Sayco
+  Date: July 2,2021
+  Time: 10:10am
+  Program: CESE
+  Course: CS10-8L
+  Section: A1
+  School: Mapua University
+%}
+
+function [WS] = SAYCO_MHIKE_MP4(W)
+WS = W*604800;
+%WS is the output variable 
+%W is the input (which is the number of weeks) 
+%604800 is the exact number of seconds in a week
+end
+
